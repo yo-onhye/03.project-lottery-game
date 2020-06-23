a7bba29095ef179061b87f434df94bf2ca8af3c8
yo-onhye <woony0807@gmail.com>
2020-06-23 23:46:07 +0900

    Updates

---
+++ precache-manifest.112680dc186a40c514c75e2b1d8f5170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507ffad7173f29ccbd15cf1d19f732df",
+    "url": "/03.personal-toyproject/project-random-select/index.html"
+  },
+  {
+    "revision": "73de8b37524d626b9d07",
+    "url": "/03.personal-toyproject/project-random-select/static/css/main.74106f5d.chunk.css"
+  },
+  {
+    "revision": "7d717483908d7e8d2437",
+    "url": "/03.personal-toyproject/project-random-select/static/js/2.4921678d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/03.personal-toyproject/project-random-select/static/js/2.4921678d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73de8b37524d626b9d07",
+    "url": "/03.personal-toyproject/project-random-select/static/js/main.67d06b8e.chunk.js"
+  },
+  {
+    "revision": "854448e532845a70aca9",
+    "url": "/03.personal-toyproject/project-random-select/static/js/runtime-main.847fcbc6.js"
+  }
+]);
